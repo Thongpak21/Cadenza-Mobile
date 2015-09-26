a32052c54280b3002b3b928f9836eeaad373797f
Thongpak Pongsilathong <thongpak21@gmail.com>
2015-09-26 09:45:01 +0700

    Test Autolayout

---
+++ Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by Thongpak on 9/24/2558 BE.
+//  Copyright © 2558 Thongpak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var loginFirld: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+}
+
