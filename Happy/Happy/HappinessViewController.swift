e610da48825bbb8ca0e19960962beaf6d5fa44ea
Thongpak Pongsilathong <thongpak21@gmail.com>
2015-09-27 16:20:32 +0700

    no message

---
+++ Happy/Happy/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happy
+//
+//  Created by Thongpak on 9/27/2558 BE.
+//  Copyright © 2558 Thongpak. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
---
+++ Happy/Happy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happy
+//
+//  Created by Thongpak on 9/27/2558 BE.
+//  Copyright © 2558 Thongpak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
