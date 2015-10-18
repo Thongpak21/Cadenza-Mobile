e47dd887161cb68872083f789ecdab1636fec0fd
Thongpak Pongsilathong <thongpak21@gmail.com>
2015-10-18 14:24:44 +0700

    no message

---
+++ Coredata/Coredata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coredata
+//
+//  Created by Thongpak on 10/5/2558 BE.
+//  Copyright © 2558 Thongpak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Psychologist/Psychologist/PsychologistViewController.swift
+++ Psychologist/Psychologist/PsychologistViewController.swift
@@ -9,17 +9,11 @@
 import UIKit
 
 class PsychologistViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let hvc = segue.destinationViewController as? HappinessViewController {
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
