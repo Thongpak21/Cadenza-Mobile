41d45e4cd0a51445959034aef55681a2feb2dfb3
Thongpak Pongsilathong <thongpak21@gmail.com>
2015-11-27 20:42:57 +0700

    Cadenza 1.2 Appdelegate & cordata

--- Cadenza/Cadenza/Login.swift
+++ Cadenza/Cadenza/Login.swift
@@ -9,29 +9,15 @@
 import UIKit
 import AlamofireImage
 import Alamofire
+import SwiftyJSON
+import CoreData
 class Login: UIViewController {
 
-    @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         LoginController()
-        Alamofire.request(.GET, "http://www.cadenza.in.th/images/Logo_Cadenza_8.png")
-            .responseImage { response in
-//                debugPrint(response)
-//                print(response.request)
-//                print(response.response)
-//                debugPrint(response.result)
-                
-                if let image = response.result.value {
-                    print("image downloaded: \(image)")
-                    self.image.image = image.af_imageWithRoundedCornerRadius(20.0)
-//                     self.image.image = image.af_imageRoundedIntoCircle()
-                    
-                }
-               
-        }
         
     }
 
@@ -42,15 +28,42 @@ class Login: UIViewController {
     
     func LoginController() {
         password.secureTextEntry = true
+   
+
     }
     
     
     @IBAction func LoginAction(sender: UIButton) {
         
-        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Home") 
-            self.presentViewController(viewController, animated: true, completion: nil)
-        })
+        
+        let data = [
+            "grant_type": "password",
+            "client_id":"client1id",
+            "client_secret":"client1secret",
+            //  "username":"demo3@cadenza.in.th",
+            //  "password":"demo3"
+            "username":username.text!,
+            "password":password.text!
+        ]
+        Alamofire.request(.POST, "http://cadenza.in.th/oauth/access_token",parameters:data )
+            .responseJSON { response in
+//                print("Response JSON: \(response.result.value)")
+                let json = JSON(response.result.value!)
+                print(json["access_token"])
+//                for (key,json):(String, JSON) in json {
+//                    print("\(key) -> \(json)")
+//                }
+                if json["access_token"] == nil {
+                    
+                } else {
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Home")
+                        self.presentViewController(viewController, animated: true, completion: nil)
+                    })
+                }
+                
+        }
+        
     }
 
     
