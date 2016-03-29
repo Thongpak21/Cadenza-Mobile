64a3f5b62cd805463ef1df26b643e9e99c7bac1f
Thongpak Pongsilathong <thongpak21@gmail.com>
2016-03-29 19:46:40 +0700

    Cadenza No have SWreal

--- Cadenza/Cadenza/Cadenza-Bridging-Header.h
+++ Cadenza/Cadenza/Cadenza-Bridging-Header.h
@@ -2,5 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "SWRevealViewController.h"
 #import <UIScrollView_InfiniteScroll/UIScrollView+InfiniteScroll.h>
