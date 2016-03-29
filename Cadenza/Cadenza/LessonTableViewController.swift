cd81ed92fa0d1cd3759257ae45697dc81242b95b
Thongpak Pongsilathong <thongpak21@gmail.com>
2016-03-29 19:45:44 +0700

    Cadenza With Swrea

--- Cadenza/Cadenza/LessonTableViewController.swift
+++ Cadenza/Cadenza/LessonTableViewController.swift
@@ -46,7 +46,6 @@ class LessonTableViewController: UITableViewController {
         
     }
 
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
